17138155a881c1e2119b83d2d72d35293b717c7d
Samuel Sarle <samuel@sarle.com>
2025-09-18 07:41:03 +0300

    Implement asimov-gemini-prompter

---
+++ README.md
@@ -0,0 +1,85 @@
+# ASIMOV Gemini Module
+
+[![License](https://img.shields.io/badge/license-Public%20Domain-blue.svg)](https://unlicense.org)
+[![Package on Crates.io](https://img.shields.io/crates/v/asimov-gemini-module)](https://crates.io/crates/asimov-gemini-module)
+[![Documentation](https://docs.rs/asimov-gemini-module/badge.svg)](https://docs.rs/asimov-gemini-module)
+
+[ASIMOV] Gemini module.
+
+## ✨ Features
+
+- To be determined!
+
+## 🛠️ Prerequisites
+
+- [Rust] 1.85+ (2024 edition) if building from source code
+
+## ⬇️ Installation
+
+### Installation with [ASIMOV CLI]
+
+```bash
+asimov module install gemini -v
+```
+
+### Installation from Source Code
+
+```bash
+cargo install asimov-gemini-module
+```
+
+## 👉 Examples
+
+```bash
+asimov-gemini-prompter
+```
+
+## ⚙ Configuration
+
+Provide an API key either by module configuration
+
+```bash
+asimov module config gemini
+```
+
+Or through environment variables
+
+```bash
+export GEMINI_API_KEY="..."
+```
+
+### Optional configuration
+
+| Name       | Environment Variable  | Default                                     |
+| ---------- | --------------------- | ------------------------------------------- |
+| `endpoint` | `GEMINI_API_ENDPOINT` | `https://generativelanguage.googleapis.com` |
+| `model`    | `GEMINI_MODEL`        | `gemini-2.5-flash`                          |
+
+## 📚 Reference
+
+### Prompt
+
+```bash
+echo "Why is the sky blue?" | asimov-gemini-prompter
+```
+
+## 👨‍💻 Development
+
+```bash
+git clone https://github.com/asimov-modules/asimov-gemini-module.git
+```
+
+---
+
+[![Share on X](https://img.shields.io/badge/share%20on-x-03A9F4?logo=x)](https://x.com/intent/post?url=https://github.com/asimov-modules/asimov-gemini-module&text=asimov-gemini-module)
+[![Share on Reddit](https://img.shields.io/badge/share%20on-reddit-red?logo=reddit)](https://reddit.com/submit?url=https://github.com/asimov-modules/asimov-gemini-module&title=asimov-gemini-module)
+[![Share on Hacker News](https://img.shields.io/badge/share%20on-hn-orange?logo=ycombinator)](https://news.ycombinator.com/submitlink?u=https://github.com/asimov-modules/asimov-gemini-module&t=asimov-gemini-module)
+[![Share on Facebook](https://img.shields.io/badge/share%20on-fb-1976D2?logo=facebook)](https://www.facebook.com/sharer/sharer.php?u=https://github.com/asimov-modules/asimov-gemini-module)
+[![Share on LinkedIn](https://img.shields.io/badge/share%20on-linkedin-3949AB?logo=linkedin)](https://www.linkedin.com/sharing/share-offsite/?url=https://github.com/asimov-modules/asimov-gemini-module)
+
+[ASIMOV]: https://asimov.sh
+[ASIMOV CLI]: https://cli.asimov.sh
+[JSON-LD]: https://json-ld.org
+[KNOW]: https://know.dev
+[RDF]: https://www.w3.org/TR/rdf12-primer/
+[Rust]: https://rust-lang.org
---
+++ src/lib.rs
@@ -0,0 +1,133 @@
+// This is free and unencumbered software released into the public domain.
+
+#![no_std]
+#![forbid(unsafe_code)]
+
+use asimov_module::{
+    prelude::*,
+    secrecy::{ExposeSecret, SecretString},
+    tracing,
+};
+use core::error::Error;
+use serde_json::{Value, json};
+
+#[derive(Clone, Debug, bon::Builder)]
+#[builder(on(String, into))]
+pub struct Options {
+    #[builder(default = "https://generativelanguage.googleapis.com")]
+    pub endpoint: String,
+
+    #[builder(default = "gemini-2.5-flash")]
+    pub model: String,
+
+    #[builder(default = 1024)]
+    pub max_tokens: usize,
+
+    #[builder(into)]
+    pub api_key: SecretString,
+}
+
+pub fn generate(input: impl AsRef<str>, options: &Options) -> Result<Vec<String>, Box<dyn Error>> {
+    let req = json!({
+        "contents": {
+            "parts": [
+                {"text": input.as_ref()}
+            ]
+        },
+    });
+
+    let mut resp = ureq::Agent::config_builder()
+        .http_status_as_error(false)
+        .user_agent("asimov-gemini-module")
+        .build()
+        .new_agent()
+        .post(format!(
+            "{}/v1beta/models/{}:generateContent",
+            options.endpoint, options.model
+        ))
+        .header("x-goog-api-key", options.api_key.expose_secret())
+        .header("content-type", "application/json")
+        .send_json(&req)
+        .inspect_err(|e| tracing::error!("HTTP request failed: {e}"))?;
+    tracing::debug!(response = ?resp);
+
+    let status = resp.status();
+    tracing::debug!(status = status.to_string());
+
+    let resp: Value = resp
+        .body_mut()
+        .read_json()
+        .inspect_err(|e| tracing::error!("unable to read HTTP response body: {e}"))?;
+    tracing::debug!(body = ?resp);
+
+    if !status.is_success() {
+        tracing::error!("Received an error response: {status}");
+
+        // {
+        //   "error": {
+        //     "code": 400,
+        //     "message": "API key not valid. Please pass a valid API key.",
+        //     "status": "INVALID_ARGUMENT"
+        //   }
+        // }
+        if let Some(message) = resp["error"]["message"].as_str() {
+            return Err(message.into());
+        }
+    }
+
+    // {
+    //   "candidates": [
+    //     {
+    //       "content": {
+    //         "parts": [
+    //           {
+    //             "text": "..."
+    //           }
+    //         ],
+    //         "role": "model"
+    //       },
+    //       "finishReason": "STOP",
+    //       "index": 0
+    //     }
+    //   ],
+    //   "usageMetadata": {
+    //     "promptTokenCount": 8,
+    //     "candidatesTokenCount": 15,
+    //     "totalTokenCount": 1191,
+    //     "promptTokensDetails": [
+    //       {
+    //         "modality": "TEXT",
+    //         "tokenCount": 8
+    //       }
+    //     ],
+    //     "thoughtsTokenCount": 1168
+    //   },
+    //   "modelVersion": "gemini-2.5-flash",
+    //   "responseId": "..."
+    // }
+
+    let mut responses = Vec::new();
+
+    if let Some(chunks) = resp["candidates"].as_array() {
+        for chunk in chunks {
+            if let Some(content) = chunk["content"].as_object() {
+                if content["role"].as_str().is_none_or(|r| r != "model") {
+                    continue;
+                }
+
+                content["parts"]
+                    .as_array()
+                    .into_iter()
+                    .flatten()
+                    .filter_map(|p| p["text"].as_str())
+                    .for_each(|t| responses.push(t.to_string()));
+            };
+
+            if let Some(stop_reason) = chunk["finishReason"].as_str() {
+                tracing::debug!(stop_reason);
+            }
+        }
+    }
+
+    Ok(responses)
+}
